4aba00361fa54cddcd1de95c71647766b551456c
Lee Irvine <lee@kezzi.co>
2021-05-28 10:05:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# REACT-NATIVE WEB/ANDROID/IOS BOILERPLATE
+
+Install
+
+```
+npm install
+```
+
+
+Run Web
+
+```
+./node_modules/.bin/webpack-dev-server -inline
+
+```
+
+Run iOS
+```
+react-native run-ios
+```
+
+Run Android
+```
+react-native run-android
+```
---
+++ index.web.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+
+class ReactNativeWeb extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Welcome to React Native!
+				</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#142a3d',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff',
+	},
+});
+
+AppRegistry.registerComponent('ReactNativeWeb', () => ReactNativeWeb);
+AppRegistry.runApplication('WebTest', { rootTag: document.getElementById('react-native-app') });
