927b6d6477689eb007835bd71e1b8dc1ae9fdd81
Lee Irvine <lee@kezzi.co>
2021-06-12 08:21:20 -0700

    webpack loader is not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-const webpack = require('webpack');
+const webpack = require("webpack")
+
+const path = require("path")
 
 module.exports = {
   entry: {
-		main: './index.web.js',
+		main: "./index.web.js",
 	},
 	module: {
-	#loaders are depr3cated
-	#replaced by rules
-		loaders: [{
+		rules: [{
 			test: /\.js?$/,
-			exclude: /node_modules/,
+			include: [
+				path.resolve(__dirname, "index.web.js")
+			],
+			exclude: [
+				path.resolve(__dirname, "node_modules")
+			],
 
-			loader: 'babel',
-				query: {
-					presets: ['es2015', 'react'],
-				},
+			loader: "babel",
+			options: {
+				presets: ["es2015"]
 			},
-		],
+
+			issuer: /\.css$/,
+			issuer: path.resolve(__dirname, "app"),
+			issuer: { and: [ /\.css$/, path.resolve(__dirname, "app") ] },
+			issuer: { or: [ /\.css$/, path.resolve(__dirname, "app") ] },
+			issuer: { not: [ /\.css$/ ] },
+			issuer: [ /\.css$/, path.resolve(__dirname, "app") ],
+		}]
 	},
 	resolve: {
 		alias: {
